3b79a83dfd34174190553c3cceacd2606829e70c
Maksim Fomenok <maksim.fomenok@gmail.com>
2018-05-26 20:12:46 +0300

    Added several events constants, updated package lock

--- src/server/index.js
+++ src/server/index.js
@@ -3,15 +3,14 @@
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
-var io = module.exports.io = require('socket.io')(server)
 
-const PORT = process.env.PORT || 3333
+var io = module.exports.io = require('socket.io')(server);
 
-const SocketManager = require('./SocketManager')
+const PORT = process.env.PORT || 3333;
+const SocketManager = require('./SocketManager');
 
-io.on('connection', SocketManager)
-
+io.on('connection', SocketManager);
 
 server.listen(PORT, () => {
-    console.log("Connected to PORT " + PORT)
+    console.log("Connected to PORT " + PORT);
 });
