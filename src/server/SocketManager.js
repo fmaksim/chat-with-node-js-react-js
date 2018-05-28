61a1a166d6e9899e9744e956da20bd3ed1e23ec2
Maksim Fomenok <maksim.fomenok@gmail.com>
2018-05-28 23:00:09 +0300

    Handling event for receive message

--- src/server/SocketManager.js
+++ src/server/SocketManager.js
@@ -5,7 +5,9 @@ const {
     VERIFY_USER,
     USER_CONNECTED,
     USER_DISCONNECTED,
-    LOGOUT
+    LOGOUT,
+    MESSAGE_SENT,
+    MESSAGE_RECIEVED
 } = require('../Events');
 
 const {createUser, createChat, createMessage} = require('../Objects');
@@ -13,6 +15,8 @@ let connectedUsers = {};
 
 module.exports = function (socket) {
     console.log('Socket ID:' + socket.id);
+
+    let sendMessageToChat;
 
     socket.on(VERIFY_USER, (nickname, callback) => {
         if (isUser(connectedUsers, nickname)) {
@@ -26,6 +30,7 @@ module.exports = function (socket) {
 
         connectedUsers = addUser(connectedUsers, user);
         socket.user = user;
+        sendMessageToChat = sendMessageToChatMembers(user.name);
         io.emit(USER_CONNECTED, connectedUsers);
 
     });
@@ -42,6 +47,17 @@ module.exports = function (socket) {
         io.emit(USER_DISCONNECTED, connectedUsers);
     });
 
+    socket.on(MESSAGE_SENT, (chatId, message) => {
+        sendMessageToChat(chatId, message);
+    });
+
+
+}
+
+function sendMessageToChatMembers(sender) {
+    return (chatId, message) => {
+        io.emit(`${MESSAGE_RECIEVED}-${chatId}`, createMessage({message, sender}));
+    };
 }
 
 function isUser(userList, username) {
